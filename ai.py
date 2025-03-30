eaac0c348d3c2ead6f2b0d904d9b37845dd11069
Himkamal Gohain <Him.deadman@gmail.com>
2025-03-30 13:00:05 +0000

    added repo

--- ai.py
+++ ai.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import 
 
 X = np.array([[1], [2], [3], [4], [5]])
 y = np.array([2, 4, 6, 8, 10])
@@ -8,3 +9,4 @@ model = LinearRegression()
 model.fit(X, y)
 
 print(f"Prediction for 6: {model.predict([[6]])[0]}")
+print("complete")
