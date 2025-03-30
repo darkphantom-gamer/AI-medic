a996115df2ed36ef0474fd4e45e96a51249eed82
Himkamal Gohain <Him.deadman@gmail.com>
2025-03-30 13:01:56 +0000

    updated

--- ai.py
+++ ai.py
@@ -1,12 +1,3 @@
-from sklearn.linear_model import LinearRegression
-import numpy as np
-import 
+import tensorflow as tf
+import numpy
 
-X = np.array([[1], [2], [3], [4], [5]])
-y = np.array([2, 4, 6, 8, 10])
-
-model = LinearRegression()
-model.fit(X, y)
-
-print(f"Prediction for 6: {model.predict([[6]])[0]}")
-print("complete")
